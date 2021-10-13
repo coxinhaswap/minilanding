c0eb2dbe8a6e7ced0261cd88a4e635bd77df39b7
Gabriel <gabrielnabas@id.uff.br>
2021-10-13 18:07:23 -0300

    constants links

---
+++ constants/links.ts
@@ -0,0 +1,19 @@
+interface LinkProps {
+  name: string,
+  href: string
+}
+
+export const LINKS: LinkProps[] = [
+  {
+    name: 'Telegram',
+    href: 'https://t.me/CoxinhaSwap'
+  },
+  {
+    name: 'Twitter',
+    href: 'https://twitter.com/CoxinhaSwap'
+  },
+  {
+    name: 'Github',
+    href: 'https://github.com/CoxinhaSwap'
+  },
+]
