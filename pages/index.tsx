0d8f4353c3377ff25792393ce9a3df98da41805f
Gabriel <gabrielnabas@id.uff.br>
2021-10-13 18:08:33 -0300

    links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { Button } from '../components/Button'
 import Image from '../components/Image'
 import { Location } from '../components/Location'
+import { LINKS } from '../constants/links'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -19,8 +22,12 @@ const Home: NextPage = () => {
         <meta name="msapplication-TileColor" content="#f6ff9f"/>
         <meta name="theme-color" content="#fffecf"></meta>
       </Head>
-    <Location main={"https://www.coxinhaswap.finance/"}/>
+    <Location main={process.env.NEXT_PUBLIC_MAIN_PAGE as string}/>
     <Image src={'coxinhaswap.png'} href={'https://t.me/CoxinhaSwap'}/>
+
+    {LINKS.map( link => (
+      <Button href={link.href} name={link.name}/>
+    ))}
     </div>
   )
 }
